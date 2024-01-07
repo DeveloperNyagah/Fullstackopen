4a0cd53bb712b5792d501f4c2417824d96643ece
Dnyagah2947 <dennisnyagah16@gmail.com>
2024-01-07 18:09:14 +0300

    Implement bug fix for issue #123

---
+++ part1/courseinfo/reactApp/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+
+const App = () => {
+ const course = 'Half Stack application development'
+ const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+ ]
+ const total = parts.reduce((acc, part) => acc + part.exercises, 0)
+
+ return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total total={total} />
+    </div>
+ )
+}
+
+export default App
+   
---
+++ part1/courseinfo/reactApp/src/components/Content.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+export default Content;
